565eded4690b5f640e223c07558a97f6912ed190
zibokapi <zibokapi@gmail.com>
2023-09-20 16:27:37 +0800

    added tests

--- haha.py
+++ haha.py
@@ -1,2 +1,8 @@
-def brute_force():
-    ...
+def fibonacci(n: int) -> int:
+    # make this better
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import unittest
+from haha import fibonacci
 
 class TestHaha(unittest.TestCase):
     def test(self):
-        ...
+        self.assertEqual(fibonacci(0), 0)
+        self.assertEqual(fibonacci(1), 1)
+        self.assertEqual(fibonacci(2), 1)
+        self.assertEqual(fibonacci(5), 5)
+        self.assertEqual(fibonacci(10), 55)
